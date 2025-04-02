a8b5a3ee94d14003dc38c4306b390c7931f16ec8
DvdWHan <D.vanderWielen1@student.han.nl>
2025-04-02 13:35:08 +0200

    Renamed some variable names so they're english

--- triptop/src/main/java/han/triptop/strategy/util/FlightMapper.java
+++ triptop/src/main/java/han/triptop/strategy/util/FlightMapper.java
@@ -13,15 +13,15 @@ public class FlightMapper {
     Map<String, Object> responseMap = mapper.readValue(response, Map.class);
     Map<String, Object> firstItem = (Map<String, Object>)((List<Object>)responseMap.get("results")).get(0);
     Map<String, Object> detail = (Map<String, Object>)firstItem.get("detail");
-    FlightDetails vluchtDetails = new FlightDetails();
-    vluchtDetails.setFlightNumber((String)detail.get("flight"));
-    vluchtDetails.setCallSign((String)detail.get("callsign"));
-    vluchtDetails.setAirlineName((String)firstItem.get("label"));
-    vluchtDetails.setAircraftModel((String)detail.get("ac_type"));
-    vluchtDetails.setDepartureAirport((String)detail.get("schd_from"));
-    vluchtDetails.setArrivalAirport((String)detail.get("schd_to"));
-    vluchtDetails.setStatus("live".equals(firstItem.get("type")) ? "In Flight" : "Scheduled");
-    return vluchtDetails;
+    FlightDetails flightDetails = new FlightDetails();
+    flightDetails.setFlightNumber((String)detail.get("flight"));
+    flightDetails.setCallSign((String)detail.get("callsign"));
+    flightDetails.setAirlineName((String)firstItem.get("label"));
+    flightDetails.setAircraftModel((String)detail.get("ac_type"));
+    flightDetails.setDepartureAirport((String)detail.get("schd_from"));
+    flightDetails.setArrivalAirport((String)detail.get("schd_to"));
+    flightDetails.setStatus("live".equals(firstItem.get("type")) ? "In Flight" : "Scheduled");
+    return flightDetails;
   }
 
   public static FlightDetails mapFromAeroDataBox(String response) throws Exception {
@@ -31,14 +31,14 @@ public class FlightMapper {
     Map<String, Object> arrival = (Map<String, Object>)firstItem.get("arrival");
     Map<String, Object> aircraft = (Map<String, Object>)firstItem.get("aircraft");
     Map<String, Object> airline = (Map<String, Object>)firstItem.get("airline");
-    FlightDetails vluchtDetails = new FlightDetails();
-    vluchtDetails.setFlightNumber((String)firstItem.get("number"));
-    vluchtDetails.setCallSign((String)firstItem.get("callSign"));
-    vluchtDetails.setAirlineName((String)airline.get("name"));
-    vluchtDetails.setAircraftModel((String)aircraft.get("model"));
-    vluchtDetails.setDepartureAirport((String)((Map<String, Object>)departure.get("airport")).get("name"));
-    vluchtDetails.setArrivalAirport((String)((Map<String, Object>)arrival.get("airport")).get("name"));
-    vluchtDetails.setStatus((String)firstItem.get("status"));
-    return vluchtDetails;
+    FlightDetails flightDetails = new FlightDetails();
+    flightDetails.setFlightNumber((String)firstItem.get("number"));
+    flightDetails.setCallSign((String)firstItem.get("callSign"));
+    flightDetails.setAirlineName((String)airline.get("name"));
+    flightDetails.setAircraftModel((String)aircraft.get("model"));
+    flightDetails.setDepartureAirport((String)((Map<String, Object>)departure.get("airport")).get("name"));
+    flightDetails.setArrivalAirport((String)((Map<String, Object>)arrival.get("airport")).get("name"));
+    flightDetails.setStatus((String)firstItem.get("status"));
+    return flightDetails;
   }
 }
