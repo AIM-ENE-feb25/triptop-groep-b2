41775aa0e21e73d4063a0d5c70ce27de60ab524f
DvdWHan <D.vanderWielen1@student.han.nl>
2025-04-02 13:52:50 +0200

    Update to lombok-style getters and setters

--- triptop/src/main/java/han/triptop/strategy/config/StrategyConfig.java
+++ triptop/src/main/java/han/triptop/strategy/config/StrategyConfig.java
@@ -1,16 +1,14 @@
 package han.triptop.strategy.config;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.context.annotation.Configuration;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Configuration
 public class StrategyConfig {
   private String currentStrategy = "AeroDataBoxProvider"; // Default strategy
-
-  public String getCurrentStrategy() {
-    return currentStrategy;
-  }
-
-  public void setCurrentStrategy(String currentStrategy) {
-    this.currentStrategy = currentStrategy;
-  }
 }
